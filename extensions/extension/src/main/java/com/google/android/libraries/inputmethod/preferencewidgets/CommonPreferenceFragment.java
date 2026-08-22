79664e4bda2a43e5198cb0c7f4a3cf8aabe9b145
Evee <eveelogy@gmail.com>
2026-08-22 13:07:56 -0400

    fix: implement aB layout binding in MorpheSettingsFragment to prevent sub-menu crash

--- extensions/extension/src/main/java/com/google/android/libraries/inputmethod/preferencewidgets/CommonPreferenceFragment.java
+++ extensions/extension/src/main/java/com/google/android/libraries/inputmethod/preferencewidgets/CommonPreferenceFragment.java
@@ -9,6 +9,7 @@ public class CommonPreferenceFragment extends PreferenceFragmentCompat {
     public PreferenceScreen n() { return getPreferenceScreen(); }
     public Context x() { return getContext(); }
     public Activity E() { return getActivity(); }
+    public int aB() { return 0; }
     protected void aC() {}
     @Override
     public void onCreatePreferences(android.os.Bundle savedInstanceState, String rootKey) {}
