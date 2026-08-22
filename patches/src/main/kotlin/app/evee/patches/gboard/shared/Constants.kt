64771aae9d7705167f4a9ddd039cd53eec9e5336
Evee <eveelogy@gmail.com>
2026-08-22 10:26:53 -0400

    chore: remove redundant clone patch

--- patches/src/main/kotlin/app/evee/patches/gboard/shared/Constants.kt
+++ patches/src/main/kotlin/app/evee/patches/gboard/shared/Constants.kt
@@ -6,7 +6,6 @@ import app.morphe.patcher.patch.Compatibility
 
 object Constants {
     const val GBOARD_PACKAGE_NAME = "com.google.android.inputmethod.latin"
-    const val GBOARD_CLONED_PACKAGE_NAME = "com.google.android.inputmethod.latin.morphe"
     const val EXTENSION_CLASS_GBOARD = "Lapp/evee/extension/gboard/GboardSettings;"
 
     val COMPATIBILITY_GBOARD = Compatibility(
